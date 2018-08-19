4ca6208ab428c7eb7457019db952a7d2846bb232
Patricia Carter <patricia4194@gmail.com>
2018-08-19 12:26:09 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  
+  # Each sentence needs to be converted to an array 
+  # of numbers based on the indexes in the esperanto 
+  # alphabet. First, let's iterate through the array of
+  # sentence, then each individual sentence.
+  
+  numeric_sentence = []
+  sentence_hash = {}
+  
+  arr.each do |sentence|
+    sentence.split('').each do |letter|
+      numeric_sentence << esperanto_alphabet.index("#{letter}")
+    end
+    
+    sentence_hash[sentence] = numeric_sentence
+    
+  end
+  
+  minimum_length = sentence_hash.values.min.length
+  
+  sentence_hash.map do |sentence, array|
+    array.slice(0, minimum_length)
+  end
+  
+  sentence_hash = sentence_hash.sort_by{|sentence, numbers| numbers}.to_h
+  
+  return sentence_hash.keys
+
 end
